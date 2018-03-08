b8fc8a920925e9c728b9fff32c7e8a80abeb2c39
SgtDomo <domo.baron@aon.at>
2018-03-08 20:32:09 +0100

    forgot to add julia set calculator file in previous commits

---
+++ FractalVisualizer/FractalVisualizer/CustomCommands.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Input;
+
+namespace FractalVisualizer
+{
+    public static class CustomCommands
+    {
+        public static RoutedCommand GifExport = new RoutedCommand();
+    }
+}
---
+++ FractalVisualizer/FractalVisualizer/FractalCalculator/JuliaCalculator.cs
@@ -0,0 +1,26 @@
+﻿namespace FractalVisualizer.FractalCalculator
+{
+    /// <summary>
+    /// https://en.wikipedia.org/wiki/Julia_set
+    /// </summary>
+    public class JuliaCalculator : FractalCalculator
+    {
+        public JuliaCalculator(int maxIterations) : base("Julia Set", maxIterations)
+        {
+            
+            Cx = -0.7269;
+            Cy = 0.1889;
+        }
+
+        public double Cx { get; set; }
+
+        public double Cy { get; set; }
+
+        public override (double x, double y) GetNextZ(double x, double y, double x0, double y0)
+        {
+            var newX = x * x - y * y + Cx;
+            y = 2 * x * y + Cy;
+            return (newX, y);
+        }
+    }
+}

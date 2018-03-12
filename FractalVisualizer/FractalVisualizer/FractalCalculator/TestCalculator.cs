a0855f2622698772686d19da82a76e59c6a9346f
Dominik Baron <domo.baron@aon.at>
2018-03-12 16:45:22 +0100

    added a calculator for trying new fractals

---
+++ FractalVisualizer/FractalVisualizer/FractalCalculator/TestCalculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FractalVisualizer.FractalCalculator
+{
+    class TestCalculator : FractalCalculator
+    {
+        public TestCalculator(int maxIterations) : base("Test", maxIterations)
+        {
+        }
+
+        public override (double x, double y) GetNextZ(double x, double y, double x0, double y0)
+        {
+            x = x - x0;
+            y = y + y0;
+            double newX = (x * x - y * y) + x0;
+            y = 2 * x * y - y0;
+            return (newX, y);
+        }
+    }
+}
